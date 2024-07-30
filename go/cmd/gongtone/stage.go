ea42bdd3ab1f90dad65df927ed08dc05e83c4e63
peugeot <thomaspeugeot@yahoo.fr>
2024-07-30 22:49:48 +0200

    it works !

--- go/cmd/gongtone/stage.go
+++ go/cmd/gongtone/stage.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/fullstack-lang/gongtone/go/models"
+
 	// injection point for ident package import declaration{{ImportPackageDeclaration}}
 )
 
@@ -11,7 +12,7 @@ import (
 // if there are no elements in the stage to marshall
 var _ time.Time
 
-// _ point for meta package dummy declaration{{ImportPackageDummyDeclaration}}
+// Injection point for meta package dummy declaration{{ImportPackageDummyDeclaration}}
 
 // When parsed, those maps will help with the renaming process
 var _ map[string]any = map[string]any{
@@ -23,11 +24,23 @@ func _(stage *models.StageStruct) {
 
 	// Declaration of instances to stage
 
-	__Tone__000000_A := (&models.Note{Name: `A`}).Stage(stage)
+	__Freqency__000000_A4 := (&models.Freqency{Name: `A4`}).Stage(stage)
+	__Freqency__000001_E5 := (&models.Freqency{Name: `E5`}).Stage(stage)
+
+	__Note__000000_A4_E5 := (&models.Note{Name: `A4+E5`}).Stage(stage)
 
 	// Setup of values
 
-	__Tone__000000_A.Name = `A`
+	__Freqency__000000_A4.Name = `A4`
+
+	__Freqency__000001_E5.Name = `E5`
+
+	__Note__000000_A4_E5.Name = `A4+E5`
+	__Note__000000_A4_E5.Start = 0.000000
+	__Note__000000_A4_E5.Duration = 1.000000
+	__Note__000000_A4_E5.Velocity = 1.000000
 
 	// Setup of pointers
+	__Note__000000_A4_E5.Frequencies = append(__Note__000000_A4_E5.Frequencies, __Freqency__000000_A4)
+	__Note__000000_A4_E5.Frequencies = append(__Note__000000_A4_E5.Frequencies, __Freqency__000001_E5)
 }
--- ng-github.com-fullstack-lang-gongtone/projects/gongtonespecific/src/lib/gongtone/gongtone.component.ts
+++ ng-github.com-fullstack-lang-gongtone/projects/gongtonespecific/src/lib/gongtone/gongtone.component.ts
@@ -49,23 +49,21 @@ export class GongtoneComponent implements OnInit {
 
     if (this.synth) {
 
-      // // create two monophonic synths
-      // const synthA = new Tone.FMSynth().toDestination();
-      // const synthB = new Tone.AMSynth().toDestination();
-      // //play a note every quarter-note
-      // const loopA = new Tone.Loop((time) => {
-      //   synthA.triggerAttackRelease("C2", "8n", time);
-      // }, "4n").start(0);
-      // //play another note every off quarter-note, by starting it "8n"
-      // const loopB = new Tone.Loop((time) => {
-      //   synthB.triggerAttackRelease("C4", "8n", time);
-      // }, "4n").start("8n");
-      // // all loops start when the Transport is started
-      // Tone.getTransport().start();
-      // // ramp up to 800 bpm over 10 seconds
-      // Tone.getTransport().bpm.rampTo(800, 10);
-
       const now = Tone.now();
+
+      for (let note of this.frontRepo!.getFrontArray<gongtone.Note>(gongtone.Note.GONGSTRUCT_NAME)) {
+
+        var frequencies = new Array<string>()
+        for (let freq of note.Frequencies) {
+          frequencies.push(freq.Name)
+        }
+
+        // console.log(now, now + note.Start, now + note.Start + note.Duration)
+        this.synth.triggerAttack(frequencies, now + note.Start, note.Velocity)
+        this.synth.triggerRelease(frequencies, now + note.Start + note.Duration)
+
+      }
+
 
       // this.synth.triggerAttack(["D4", "A5"], now, 0.5);
       // this.synth.triggerAttack("F4", now + 0.5);
@@ -77,58 +75,8 @@ export class GongtoneComponent implements OnInit {
 
       // this.synth.triggerRelease(["D4", "A5"], now + 2);
 
-      // Tone.start()
+      Tone.start()
 
-      // const player = new Tone.Player(
-      //   "https://tonejs.github.io/audio/berklee/gong_1.mp3"
-      // ).toDestination();
-      // Tone.loaded().then(() => {
-      //   player.start();
-      // });
-
-      // const sampler = new Tone.Sampler({
-      //   urls: {
-      //     C4: "C4.mp3",
-      //     "D#4": "Ds4.mp3",
-      //     "F#4": "Fs4.mp3",
-      //     A4: "A4.mp3",
-      //   },
-      //   release: 1,
-      //   baseUrl: "https://tonejs.github.io/audio/salamander/",
-      // }).toDestination();
-
-      // Tone.loaded().then(() => {
-      //   sampler.triggerAttackRelease(["Eb4", "G4", "Bb4"], 4);
-      // });
-
-      // const player = new Tone.Player({
-      //   url: "https://tonejs.github.io/audio/berklee/gurgling_theremin_1.mp3",
-      //   loop: true,
-      //   autostart: true,
-      // });
-      // //create a distortion effect
-      // const distortion = new Tone.Distortion(0.4).toDestination();
-      // //connect a player to the distortion
-      // player.connect(distortion);
-
-      // const player = new Tone.Player({
-      //   url: "https://tonejs.github.io/audio/drum-samples/loops/ominous.mp3",
-      //   autostart: true,
-      // });
-      // const filter = new Tone.Filter(400, "lowpass").toDestination();
-      // const feedbackDelay = new Tone.FeedbackDelay(0.125, 0.5).toDestination();
-
-      // // connect the player to the feedback delay and filter in parallel
-      // player.connect(filter);
-      // player.connect(feedbackDelay);
-
-      // const osc = new Tone.Oscillator().toDestination();
-      // // start at "C4"
-      // osc.frequency.value = "C4";
-      // // ramp to "C2" over 2 seconds
-      // osc.frequency.rampTo("C2", 2);
-      // // start the oscillator for 2 seconds
-      // osc.start().stop("+6");
     }
   }
 

57c341209c2903218b7e281805dae3da715a7c3d
peugeot <thomaspeugeot@yahoo.fr>
2024-08-02 09:37:11 +0200

    progress

--- ng-github.com-fullstack-lang-gongtone/projects/gongtonespecific/src/lib/gongtone/gongtone.component.ts
+++ ng-github.com-fullstack-lang-gongtone/projects/gongtonespecific/src/lib/gongtone/gongtone.component.ts
@@ -44,8 +44,13 @@ export class GongtoneComponent implements OnInit {
     )
   }
 
+  stop() {
 
-  onButtonClick() {
+    // transport must be started before it starts invoking events
+    Tone.getTransport().stop();
+  }
+
+  play() {
 
     if (this.synth) {
 
@@ -79,5 +84,36 @@ export class GongtoneComponent implements OnInit {
 
     }
   }
+
+  playHelloTone() {
+
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease("C4", "8n");
+  }
+
+  ToneSynth() {
+
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+
+    const now = Tone.now();
+    // trigger the attack immediately
+    synth.triggerAttack("C4", now);
+    // wait one second before triggering the release
+    synth.triggerRelease(now + 1);
+  }
+
+  TriggerAttackRelease() {
+
+    const synth = new Tone.Synth().toDestination();
+    const now = Tone.now();
+    synth.triggerAttackRelease("C4", "8n", now);
+    synth.triggerAttackRelease("E4", "8n", now + 0.5);
+    synth.triggerAttackRelease("G4", "8n", now + 1);
+  }
+
 
 }

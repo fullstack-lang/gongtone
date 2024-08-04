4b51d091cc131c52cdb20f0b112e5ef891699d5f
peugeot <thomaspeugeot@yahoo.fr>
2024-08-04 16:38:00 +0300

    salamander sampler

--- go/cmd/gongtone/stage.go
+++ go/cmd/gongtone/stage.go
@@ -12,7 +12,7 @@ import (
 // if there are no elements in the stage to marshall
 var _ time.Time
 
-// _ point for meta package dummy declaration{{ImportPackageDummyDeclaration}}
+// Injection point for meta package dummy declaration{{ImportPackageDummyDeclaration}}
 
 // When parsed, those maps will help with the renaming process
 var _ map[string]any = map[string]any{
@@ -24,23 +24,28 @@ func _(stage *models.StageStruct) {
 
 	// Declaration of instances to stage
 
-	__Freqency__000000_A4 := (&models.Freqency{Name: `A4`}).Stage(stage)
-	__Freqency__000001_E5 := (&models.Freqency{Name: `E5`}).Stage(stage)
+	__Freqency__000000_Bb4 := (&models.Freqency{Name: `Bb4`}).Stage(stage)
+	__Freqency__000001_Eb4 := (&models.Freqency{Name: `Eb4`}).Stage(stage)
+	__Freqency__000002_G4 := (&models.Freqency{Name: `G4`}).Stage(stage)
 
-	__Note__000000_A4_E5 := (&models.Note{Name: `A4+E5`}).Stage(stage)
+	__Note__000000_Sampler := (&models.Note{Name: `Sampler`}).Stage(stage)
 
 	// Setup of values
 
-	__Freqency__000000_A4.Name = `A4`
+	__Freqency__000000_Bb4.Name = `Bb4`
 
-	__Freqency__000001_E5.Name = `E5`
+	__Freqency__000001_Eb4.Name = `Eb4`
 
-	__Note__000000_A4_E5.Name = `A4+E5`
-	__Note__000000_A4_E5.Start = 0.000000
-	__Note__000000_A4_E5.Duration = 1.000000
-	__Note__000000_A4_E5.Velocity = 1.000000
+	__Freqency__000002_G4.Name = `G4`
+
+	__Note__000000_Sampler.Name = `Sampler`
+	__Note__000000_Sampler.Start = 0.000000
+	__Note__000000_Sampler.Duration = 4.000000
+	__Note__000000_Sampler.Velocity = 1.000000
+	__Note__000000_Sampler.Info = ``
 
 	// Setup of pointers
-	__Note__000000_A4_E5.Frequencies = append(__Note__000000_A4_E5.Frequencies, __Freqency__000000_A4)
-	__Note__000000_A4_E5.Frequencies = append(__Note__000000_A4_E5.Frequencies, __Freqency__000001_E5)
+	__Note__000000_Sampler.Frequencies = append(__Note__000000_Sampler.Frequencies, __Freqency__000001_Eb4)
+	__Note__000000_Sampler.Frequencies = append(__Note__000000_Sampler.Frequencies, __Freqency__000002_G4)
+	__Note__000000_Sampler.Frequencies = append(__Note__000000_Sampler.Frequencies, __Freqency__000000_Bb4)
 }
--- ng-github.com-fullstack-lang-gongtone/projects/gongtonespecific/src/lib/gongtonetest/gongtonetest.component.ts
+++ ng-github.com-fullstack-lang-gongtone/projects/gongtonespecific/src/lib/gongtonetest/gongtonetest.component.ts
@@ -92,6 +92,10 @@ export class GongtonetestComponent {
   Sampler() {
     const sampler = new Tone.Sampler({
       urls: {
+        C3: "C3.mp3",
+        "D#3": "Ds3.mp3",
+        "F#3": "Fs3.mp3",
+        A3: "A3.mp3",
         C4: "C4.mp3",
         "D#4": "Ds4.mp3",
         "F#4": "Fs4.mp3",
@@ -102,7 +106,8 @@ export class GongtonetestComponent {
     }).toDestination();
 
     Tone.loaded().then(() => {
-      sampler.triggerAttackRelease(["Eb4", "G4", "Bb4"], 4);
+      // sampler.triggerAttackRelease(["Eb4", "G4", "Bb4"], 4);
+      sampler.triggerAttackRelease(["Eb3", "G3", "Bb3"], 2);
     });
   }
 

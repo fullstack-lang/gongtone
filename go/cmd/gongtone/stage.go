6ac58a59d1d4e81a5b68c9160a16c898d1860a66
peugeot <thomaspeugeot@yahoo.fr>
2024-08-05 10:20:51 +0300

    loop version

--- go/cmd/gongtone/stage.go
+++ go/cmd/gongtone/stage.go
@@ -24,28 +24,110 @@ func _(stage *models.StageStruct) {
 
 	// Declaration of instances to stage
 
-	__Freqency__000000_Bb4 := (&models.Freqency{Name: `Bb4`}).Stage(stage)
-	__Freqency__000001_Eb4 := (&models.Freqency{Name: `Eb4`}).Stage(stage)
-	__Freqency__000002_G4 := (&models.Freqency{Name: `G4`}).Stage(stage)
+	__Freqency__000000_B4 := (&models.Freqency{Name: `B4`}).Stage(stage)
+	__Freqency__000001_Bb4 := (&models.Freqency{Name: `Bb4`}).Stage(stage)
+	__Freqency__000002_C5 := (&models.Freqency{Name: `C5`}).Stage(stage)
+	__Freqency__000003_D5 := (&models.Freqency{Name: `D5`}).Stage(stage)
+	__Freqency__000004_Eb4 := (&models.Freqency{Name: `Eb4`}).Stage(stage)
+	__Freqency__000005_G_4 := (&models.Freqency{Name: `G#4`}).Stage(stage)
+	__Freqency__000006_G4 := (&models.Freqency{Name: `G4`}).Stage(stage)
 
-	__Note__000000_Sampler := (&models.Note{Name: `Sampler`}).Stage(stage)
+	__Note__000000_1_C5 := (&models.Note{Name: `1 C5`}).Stage(stage)
+	__Note__000001_10_G4 := (&models.Note{Name: `10. G4`}).Stage(stage)
+	__Note__000002_2_B4 := (&models.Note{Name: `2. B4`}).Stage(stage)
+	__Note__000003_3_C5 := (&models.Note{Name: `3. C5`}).Stage(stage)
+	__Note__000004_4_G4 := (&models.Note{Name: `4. G4`}).Stage(stage)
+	__Note__000005_5_G4_ := (&models.Note{Name: `5. G4#`}).Stage(stage)
+	__Note__000006_6_C5 := (&models.Note{Name: `6. C5`}).Stage(stage)
+	__Note__000007_7_B4 := (&models.Note{Name: `7. B4`}).Stage(stage)
+	__Note__000008_8_C5 := (&models.Note{Name: `8. C5`}).Stage(stage)
+	__Note__000009_9_D5 := (&models.Note{Name: `9. D5`}).Stage(stage)
 
 	// Setup of values
 
-	__Freqency__000000_Bb4.Name = `Bb4`
+	__Freqency__000000_B4.Name = `B4`
 
-	__Freqency__000001_Eb4.Name = `Eb4`
+	__Freqency__000001_Bb4.Name = `Bb4`
 
-	__Freqency__000002_G4.Name = `G4`
+	__Freqency__000002_C5.Name = `C5`
 
-	__Note__000000_Sampler.Name = `Sampler`
-	__Note__000000_Sampler.Start = 0.000000
-	__Note__000000_Sampler.Duration = 4.000000
-	__Note__000000_Sampler.Velocity = 1.000000
-	__Note__000000_Sampler.Info = ``
+	__Freqency__000003_D5.Name = `D5`
+
+	__Freqency__000004_Eb4.Name = `Eb4`
+
+	__Freqency__000005_G_4.Name = `G#4`
+
+	__Freqency__000006_G4.Name = `G4`
+
+	__Note__000000_1_C5.Name = `1 C5`
+	__Note__000000_1_C5.Start = 0.000000
+	__Note__000000_1_C5.Duration = 0.500000
+	__Note__000000_1_C5.Velocity = 0.000000
+	__Note__000000_1_C5.Info = ``
+
+	__Note__000001_10_G4.Name = `10. G4`
+	__Note__000001_10_G4.Start = 7.000000
+	__Note__000001_10_G4.Duration = 1.000000
+	__Note__000001_10_G4.Velocity = 0.000000
+	__Note__000001_10_G4.Info = ``
+
+	__Note__000002_2_B4.Name = `2. B4`
+	__Note__000002_2_B4.Start = 0.500000
+	__Note__000002_2_B4.Duration = 0.500000
+	__Note__000002_2_B4.Velocity = 0.000000
+	__Note__000002_2_B4.Info = ``
+
+	__Note__000003_3_C5.Name = `3. C5`
+	__Note__000003_3_C5.Start = 1.000000
+	__Note__000003_3_C5.Duration = 1.000000
+	__Note__000003_3_C5.Velocity = 0.000000
+	__Note__000003_3_C5.Info = ``
+
+	__Note__000004_4_G4.Name = `4. G4`
+	__Note__000004_4_G4.Start = 2.000000
+	__Note__000004_4_G4.Duration = 1.000000
+	__Note__000004_4_G4.Velocity = 0.000000
+	__Note__000004_4_G4.Info = ``
+
+	__Note__000005_5_G4_.Name = `5. G4#`
+	__Note__000005_5_G4_.Start = 3.000000
+	__Note__000005_5_G4_.Duration = 1.000000
+	__Note__000005_5_G4_.Velocity = 0.000000
+	__Note__000005_5_G4_.Info = ``
+
+	__Note__000006_6_C5.Name = `6. C5`
+	__Note__000006_6_C5.Start = 4.000000
+	__Note__000006_6_C5.Duration = 0.500000
+	__Note__000006_6_C5.Velocity = 0.000000
+	__Note__000006_6_C5.Info = ``
+
+	__Note__000007_7_B4.Name = `7. B4`
+	__Note__000007_7_B4.Start = 4.500000
+	__Note__000007_7_B4.Duration = 0.500000
+	__Note__000007_7_B4.Velocity = 0.000000
+	__Note__000007_7_B4.Info = ``
+
+	__Note__000008_8_C5.Name = `8. C5`
+	__Note__000008_8_C5.Start = 5.000000
+	__Note__000008_8_C5.Duration = 1.000000
+	__Note__000008_8_C5.Velocity = 0.000000
+	__Note__000008_8_C5.Info = ``
+
+	__Note__000009_9_D5.Name = `9. D5`
+	__Note__000009_9_D5.Start = 6.000000
+	__Note__000009_9_D5.Duration = 1.000000
+	__Note__000009_9_D5.Velocity = 0.000000
+	__Note__000009_9_D5.Info = ``
 
 	// Setup of pointers
-	__Note__000000_Sampler.Frequencies = append(__Note__000000_Sampler.Frequencies, __Freqency__000001_Eb4)
-	__Note__000000_Sampler.Frequencies = append(__Note__000000_Sampler.Frequencies, __Freqency__000002_G4)
-	__Note__000000_Sampler.Frequencies = append(__Note__000000_Sampler.Frequencies, __Freqency__000000_Bb4)
+	__Note__000000_1_C5.Frequencies = append(__Note__000000_1_C5.Frequencies, __Freqency__000002_C5)
+	__Note__000001_10_G4.Frequencies = append(__Note__000001_10_G4.Frequencies, __Freqency__000006_G4)
+	__Note__000002_2_B4.Frequencies = append(__Note__000002_2_B4.Frequencies, __Freqency__000000_B4)
+	__Note__000003_3_C5.Frequencies = append(__Note__000003_3_C5.Frequencies, __Freqency__000002_C5)
+	__Note__000004_4_G4.Frequencies = append(__Note__000004_4_G4.Frequencies, __Freqency__000006_G4)
+	__Note__000005_5_G4_.Frequencies = append(__Note__000005_5_G4_.Frequencies, __Freqency__000005_G_4)
+	__Note__000006_6_C5.Frequencies = append(__Note__000006_6_C5.Frequencies, __Freqency__000002_C5)
+	__Note__000007_7_B4.Frequencies = append(__Note__000007_7_B4.Frequencies, __Freqency__000000_B4)
+	__Note__000008_8_C5.Frequencies = append(__Note__000008_8_C5.Frequencies, __Freqency__000002_C5)
+	__Note__000009_9_D5.Frequencies = append(__Note__000009_9_D5.Frequencies, __Freqency__000003_D5)
 }
